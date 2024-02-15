54b20d76be66dfa65ffc6c6bb36a628f50cdcb10
ygursu <yasemingrs@gmail.com>
2024-02-15 22:50:40 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp67/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace klavyedengirilensayipozitifnegatifsifir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Klavyeden girilen sayının negatif, pozitif veya sıfır olduğunu bulan C#  programını yazın
+
+
+            Console.WriteLine("Lütfen bir sayı giriniz");
+            int sayi = Int32.Parse(Console.ReadLine());
+
+            if (sayi > 0)
+            {
+                Console.WriteLine("Sayı Pozitif");
+            }
+            else if (sayi < 0)
+            {
+                Console.WriteLine("Sayı Negatif");
+            }
+            else
+            {
+                Console.WriteLine("Sayı Sıfır");
+            }
+
+            Console.ReadLine();
+        }
